47e246fd206f027765a1e023738d5347e9118b99
Raff <rafiazhim.ra@gmail.com>
2023-11-03 05:36:49 +0700

    Initial commit

--- dashboard/js/controller/cog.js
+++ dashboard/js/controller/cog.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {map,idmarker} from '../config/peta.js';
 import {insertMarkerCOG} from './marker.js';
 import {disposePopover} from './popup.js';
@@ -38,45 +37,4 @@ export function getAllCoordinates(){
     insertMarkerCOG(x,y);
     disposePopover();
     hide('hitungcogbutton');
-=======
-import {map,idmarker} from '../config/peta.js';
-import {insertMarkerCOG} from './marker.js';
-import {disposePopover} from './popup.js';
-import {hide} from 'https://jscroot.github.io/element/croot.js';
-
-export function getAllCoordinates(){
-    let i=0;
-    let sudahhapus=0;
-    let pointlist = [];
-    let totaldemand=0;
-    let Xcog=0;
-    let Ycog=0;
-    map.getLayers().forEach(layer => {
-      if (i !== 0 && sudahhapus === 0) {
-        layer.getSource().getFeatures().forEach( feature =>
-          {
-            let node = {
-                id : feature.get('id'),
-                name : feature.get('name'),
-                volume : feature.get('volume'),
-                xy : feature.get('geometry').flatCoordinates,
-            }
-            pointlist.push(node);
-            totaldemand=totaldemand+Number(feature.get('volume'));
-            Xcog=Xcog+feature.get('geometry').flatCoordinates[0]*Number(feature.get('volume'));
-            Ycog=Ycog+feature.get('geometry').flatCoordinates[1]*Number(feature.get('volume'));
-          }
-        );
-      }
-      i++;
-    });
-    console.log(pointlist);
-    let x=Xcog/totaldemand;
-    let y=Ycog/totaldemand;
-    console.log(x);
-    console.log(y);
-    insertMarkerCOG(x,y);
-    disposePopover();
-    hide('hitungcogbutton');
->>>>>>> a08d6342e45f33b3c4e2eb8307b9af4b761a82a4
 }
--- dashboard/js/template/geocf.js
+++ dashboard/js/template/geocf.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export let URLGeoJson = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/fetchGeoJson";
 export let urlPostGCF = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postGIS4";
 export let tableTag="tr";
@@ -14,21 +13,4 @@ Lat  : #LAT#<br>
 X   : #X#<br>
 Y   : #Y#<br>
 HDMS : #HDMS#<br>
-=======
-export let URLGeoJson = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/fetchGeoJson";
-export let urlPostGCF = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postGIS4";
-export let tableTag="tr";
-export let tableRowClass="content is-small";
-export let tableTemplate=`
-<td>#NAME#</td>
-<td >#KORDINAT#</td>
-<td>#TYPE#</td>
-`
-export const clickpopup = `
-Long : #LONG#<br>
-Lat  : #LAT#<br>
-X   : #X#<br>
-Y   : #Y#<br>
-HDMS : #HDMS#<br>
->>>>>>> a08d6342e45f33b3c4e2eb8307b9af4b761a82a4
 `
