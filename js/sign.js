8021e1c85de900148fa4193be3800a5e8d731f1b
Raff <rafiazhim.ra@gmail.com>
2023-11-13 05:26:34 +0700

    update name form sign up

--- js/sign.js
+++ js/sign.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById("form");
+  const form = document.getElementById("signup-form");
 
   form.addEventListener("submit", function(event) {
       event.preventDefault();
