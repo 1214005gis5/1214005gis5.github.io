1f5b2fb9dd44648111c36a0b1c0da8fabd8fcf42
Raff <rafiazhim.ra@gmail.com>
2023-11-13 05:17:03 +0700

    update add export

--- dashboard/js/template/geocf.js
+++ dashboard/js/template/geocf.js
@@ -2,6 +2,8 @@ import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
 
 export let URLGeoJson = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/fetchGeoJson";
 export let urlPostGCF = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postGIS4";
+export let UrlLogin = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/loginGISChapter5"
+export let UrlRegister = "https://asia-southeast2-gis-project-401902.cloudfunctions.net/RegisterGeo"
 export let tableTag="tr";
 export let tableRowClass="content is-small";
 export let token = "tokennya"
