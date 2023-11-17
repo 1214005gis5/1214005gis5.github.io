4c5a9cd08737d9a3eaa094be37ae0376c965ee1c
Raff <rafiazhim.ra@gmail.com>
2023-11-17 15:12:32 +0700

    update fix import path

--- dashboard/js/controller/popup.js
+++ dashboard/js/controller/popup.js
@@ -5,7 +5,7 @@ import {URLGeoJson, clickpopup, urlPostGCF, cookie} from '../template/geocf.js';
 import {insertMarker,deleteMarker} from './marker.js';
 import {setInner,textBlur,onClick, getValue,setValue} from 'https://jscroot.github.io/element/croot.js';
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import { allCoordinates } from '../main.js';
+import { allCoordinates } from '../script.js';
 
 export function onClosePopupClick() {
     overlay.setPosition(undefined);
